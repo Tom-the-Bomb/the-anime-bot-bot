d0a179041a6660a628675ea7ed03202d82876bad
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-27 17:19:03 -0800

    botlist.space gateway

--- cogs/web.py
+++ cogs/web.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands, tasks
+import os
+import asyncio
+import time
 from contextlib import suppress
 import aiohttp
 
@@ -8,6 +11,33 @@ class web(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.func.start(bot)
+    async def main(self):
+        async with self.bot.session.ws_connect("wss://gateway.botlist.space") as ws:
+            self.bot.loop.create_task(self.identify(ws))
+            self.bot.loop.create_task(self.heartbeat(ws))
+            async for message in ws:
+                pass
+
+    async def identify(self, ws):
+        payload = {
+            "op": 0,
+            "t": time.time(),
+            "d": {
+                "tokens": [os.getenv("botlist_space")]
+            }
+        }
+        await ws.send_json(payload)
+
+    async def heartbeat(self, ws):
+        while True:
+            payload = {
+            "op": 1,
+            "t": time.time(),
+            "d": {}
+            }
+            await ws.send_json(payload)
+            await asyncio.sleep(45)
+
 
     @tasks.loop(minutes=1)
     async def func(self, bot):
@@ -17,6 +47,7 @@ class web(commands.Cog):
                 bot.botlist = await resp.text()
         except:
             pass
+        
 
 
 def setup(bot):
