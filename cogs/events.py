302f1d7829da0b74f0eee5936722748c64d9fa7a
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-28 09:35:12 -0800

    fix some emoji spam

--- cogs/events.py
+++ cogs/events.py
@@ -439,8 +439,8 @@ class events(commands.Cog):
                 if e.is_usable() != False:
                     lists.append(str(e))
                 if lists != []:
-                    message_ = await message.channel.send("".join(lists))
-                    self.bot._message_cache[message.id] = message_
+            message_ = await message.channel.send("".join(lists))
+            self.bot._message_cache[message.id] = message_
         # mentions = message.mentions
         # try:
         #   for x in mentions:
