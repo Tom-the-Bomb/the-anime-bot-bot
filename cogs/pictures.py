aeab98cbba1cfcc87d7dcbc83f387095a60ec24a
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-02 14:18:41 -0800

    change license

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -780,9 +780,9 @@ class pictures(commands.Cog):
                                          discord.Emoji, str] = None):
         async with ctx.channel.typing():
             url = await self.get_url(ctx, thing)
-        img = await self.bot.dag.image_process(ImageFeatures.invert(), url)
-        file = discord.File(fp=img.image, filename=f"pixel.{img.format}")
-        await ctx.reply(file=file)
+            img = await self.bot.dag.image_process(ImageFeatures.invert(), url)
+            file = discord.File(fp=img.image, filename=f"pixel.{img.format}")
+            await ctx.reply(file=file)
 
     @commands.command()
     async def wasted(self,
