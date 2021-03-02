673cf771fae1a009369e1719f4b3e9d12fa920b6
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-02 15:01:45 -0800

    oops no

--- cogs/utility.py
+++ cogs/utility.py
@@ -1083,7 +1083,7 @@ class utility(commands.Cog):
             member1 = ctx.author
         if member == "random":
             member1 = random.choice(ctx.guild.members)
-        embed = discord.Embed(color=self.bot.color, , description=f"[webp]({str(member1.avatar_url_as(format='png'))}) [jpeg]({str(member1.avatar_url_as(format='jpeg'))}) [png]({str(member1.avatar_url_as(format='png'))})" if not ctx.author.is_avatar_animated() else f"[webp]({str(member1.avatar_url_as(format='png'))}) [jpeg]({str(member1.avatar_url_as(format='jpeg'))}) [png]({str(member1.avatar_url_as(format='png'))}) [gif]({str(member1.avatar_url_as(format='gif'))})")
+        embed = discord.Embed(color=self.bot.color, escription=f"[webp]({str(member1.avatar_url_as(format='png'))}) [jpeg]({str(member1.avatar_url_as(format='jpeg'))}) [png]({str(member1.avatar_url_as(format='png'))})" if not ctx.author.is_avatar_animated() else f"[webp]({str(member1.avatar_url_as(format='png'))}) [jpeg]({str(member1.avatar_url_as(format='jpeg'))}) [png]({str(member1.avatar_url_as(format='png'))}) [gif]({str(member1.avatar_url_as(format='gif'))})")
         format = ".gif" if member1.is_avatar_animated() else ".png"
         embed.set_image(url=f"attachment://{member1.id}{format}")
         embed.set_footer(
