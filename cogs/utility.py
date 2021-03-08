9273b3e67242434c02c9cdffcad816644972603e
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-08 11:04:31 -0800

    aaa api mean

--- cogs/utility.py
+++ cogs/utility.py
@@ -138,38 +138,108 @@ transformer = Transformer()
 class nogooderror(Exception):
     pass
 
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+class SphinxObjectFileReader:
+    BUFSIZE = 16 * 1024
+    def __init__(self, buffer):
+        self.stream = io.BytesIO(buffer)
+    def readline(self):
+        return self.stream.readline().decode('utf-8')
+    def skipline(self):
+        self.stream.readline()
+    def read_compressed_chunks(self):
+        decompressor = zlib.decompressobj()
+        while True:
+            chunk = self.stream.read(self.BUFSIZE)
+            if len(chunk) == 0:
+                break
+            yield decompressor.decompress(chunk)
+        yield decompressor.flush()
+    def read_compressed_lines(self):
+        buf = b''
+        for chunk in self.read_compressed_chunks():
+            buf += chunk
+            pos = buf.find(b'\n')
+            while pos != -1:
+                yield buf[:pos].decode('utf-8')
+                buf = buf[pos + 1:]
+                pos = buf.find(b'\n')
 class utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.rtfm_lock = asyncio.Lock()
-        self.rtfs_lock = asyncio.Lock()
+        page_types = {
+            'latest': 'https://discordpy.readthedocs.io/en/latest',
+            'python': 'https://docs.python.org/3',
+            'asyncpg': "https://magicstack.github.io/asyncpg/current/",
+            "zaneapi": "https://docs.zaneapi.com/en/latest/",
+            "aiohttp": "https://docs.aiohttp.org/en/stable/"
+        }
+        bot.loop.create_task(
+            self.build_rtfm_lookup_table(page_types=page_types))
         bot.cse1 = cse.Search(api_key=google_api_1)
         bot.cse2 = cse.Search(api_key=google_api_2)
         bot.cse3 = cse.Search(api_key=google_api_3)
         bot.cse_lists = cycle([bot.cse1, bot.cse2, bot.cse3])
-
-    async def uhh_rtfs_pls(self, ctx, key, obj):
-        await self.rtfs_lock.acquire()
-        try:
-            async with self.bot.session.get(f"https://idevision.net/api/public/rtfs?query={obj}&libary={key}") as resp:
-                if resp.status == 429:
-                    time = resp.headers.get("ratelimit-retry-after")
-                    await asyncio.sleep(int(time))
-                    self.rtfs_lock.release()
-                    return await self.uhh_rtfs_pls(ctx, key, obj)
-                if resp.headers.get("Content-Type") == "application/octet-stream":
-                    time = resp.headers.get("ratelimit-retry-after")
-                    await asyncio.sleep(int(time))
-                    self.rtfs_lock.release()
-                    return await self.uhh_rtfs_pls(ctx, key, obj)
-                matches = await resp.json()
-                matches = matches.get("nodes")
-                embed = discord.Embed(color=self.bot.color)
-                embed.description = '\n'.join(f'[{key}]({url})' for key, url in matches.items())
-                return await ctx.send(embed=embed, reference=ctx.replied_reference)
-        finally:
-            if self.rtfs_lock.locked():
-                self.rtfs_lock.release()
+    def parse_object_inv(self, stream, url):
+        # key: URL
+        # n.b.: key doesn't have `discord` or `discord.ext.commands` namespaces
+        result = {}
+        # first line is version info
+        inv_version = stream.readline().rstrip()
+        if inv_version != '# Sphinx inventory version 2':
+            raise RuntimeError('Invalid objects.inv file version.')
+        # next line is "# Project: <name>"
+        # then after that is "# Version: <version>"
+        projname = stream.readline().rstrip()[11:]
+        version = stream.readline().rstrip()[11:]
+        # next line says if it's a zlib header
+        line = stream.readline()
+        if 'zlib' not in line:
+            raise RuntimeError(
+                'Invalid objects.inv file, not z-lib compatible.')
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(
+            r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        for line in stream.read_compressed_lines():
+            match = entry_regex.match(line.rstrip())
+            if not match:
+                continue
+            name, directive, prio, location, dispname = match.groups()
+            domain, _, subdirective = directive.partition(':')
+            if directive == 'py:module' and name in result:
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
+                # two inventory entries are created
+                # for Python modules, and the first
+                # one is correct
+                continue
+            # Most documentation pages have a label
+            if directive == 'std:doc':
+                subdirective = 'label'
+            if location.endswith('$'):
+                location = location[:-1] + name
+            key = name if dispname == '-' else dispname
+            prefix = f'{subdirective}:' if domain == 'std' else ''
+            if projname == 'discord.py':
+                key = key.replace('discord.ext.commands.',
+                                  '').replace('discord.', '')
+            result[f'{prefix}{key}'] = os.path.join(url, location)
+        return result
+    async def build_rtfm_lookup_table(self, page_types):
+        cache = {}
+        for key, page in page_types.items():
+            sub = cache[key] = {}
+            async with self.bot.session.get(page + '/objects.inv') as resp:
+                if resp.status != 200:
+                    raise RuntimeError(
+                        'Cannot build rtfm lookup table, try again later.')
+                stream = SphinxObjectFileReader(await resp.read())
+                cache[key] = self.parse_object_inv(stream, page)
+        self.bot._rtfm_cache = cache
     async def uhh_rtfm_pls(self, ctx, key, obj):
         page_types = {
             'latest': 'https://discordpy.readthedocs.io/en/latest',
@@ -178,45 +248,37 @@ class utility(commands.Cog):
             "zaneapi": "https://docs.zaneapi.com/en/latest/",
             "aiohttp": "https://docs.aiohttp.org/en/stable/"
         }
-        await self.rtfm_lock.acquire()
-        try:
-            async with self.bot.session.get(f"https://idevision.net/api/public/rtfm?query={obj}&location={page_types.get(key)}&show-labels=true&label-labels=true") as resp:
-                if resp.status == 429:
-                    time = resp.headers.get("ratelimit-retry-after")
-                    await asyncio.sleep(int(time))
-                    self.rtfm_lock.release()
-                    return await self.uhh_rtfm_pls(ctx, key, obj)
-                if resp.headers.get("Content-Type") == "application/octet-stream":
-                    time = resp.headers.get("ratelimit-retry-after")
-                    await asyncio.sleep(int(time))
-                    self.rtfm_lock.release()
-                    return await self.uhh_rtfm_pls(ctx, key, obj)
-                matches = await resp.json()
-                matches = matches.get("nodes")
-                embed = discord.Embed(color=self.bot.color)
-                embed.description = '\n'.join(f'[{key.replace("discord.", "").replace("ext.", "")}]({url})' for key, url in matches.items())
-                return await ctx.send(embed=embed, reference=ctx.replied_reference)
-        finally:
-            if self.rtfm_lock.locked():
-                self.rtfm_lock.release()
-
-    @staticmethod
-    def choosebstofcal(ctx, times, choices):
-
-        if times is None:
-            times = (len(choices)**2) + 1
-        times = min(1000000, max(1, times))
-        results = Counter(np.random.choice(choices, times))
-        builder = []
-        if len(results) > 30:
-            builder.append('Showing the top 30 results')
-        for index, (elem, count) in enumerate(results.most_common(30),
-                                              start=1):
-            builder.append(
-                f'**{index}. {elem} ** `({plural(count):time}, {count/times:.2%})`'
-            )
-        return builder
-
+        if obj is None:
+            await ctx.send(page_types[key])
+            return
+        if not hasattr(self.bot, "_rtfm_cache"):
+            await ctx.trigger_typing()
+            await self.build_rtfm_lookup_table(page_types)
+        obj = re.sub(r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1',
+                     obj)
+        if key.startswith('latest'):
+            # point the abc.Messageable types properly:
+            q = obj.lower()
+            for name in dir(discord.abc.Messageable):
+                if name[0] == '_':
+                    continue
+                if q == name:
+                    obj = f'abc.Messageable.{name}'
+                    break
+        cache = list(self.bot._rtfm_cache[key].items())
+        def transform(tup):
+            return tup[0]
+        matches = fuzzy.finder(obj, cache, key=lambda t: t[0], lazy=False)[:10]
+        e = discord.Embed(colour=0x00ff6a)
+        if len(matches) == 0:
+            return await ctx.send("Can't find anything")
+        # paginator = commands.Paginator(prefix="", suffix="", max_size=1980)
+        # for i,v in matches:
+        #     paginator.add_line(f'[{i}]({v})')
+        # interface = PaginatorEmbedInterface(ctx.bot, paginator, owner=ctx.author)
+        # await interface.send_to(ctx)
+        e.description = '\n'.join(f'[{key}]({url})' for key, url in matches)
+        await ctx.send(embed=e, reference=ctx.replied_reference)
     @staticmethod
     def convertcal(amount, from_, to):
         c = CurrencyConverter(decimal=True)
@@ -533,11 +595,6 @@ class utility(commands.Cog):
             await ctx.send(embed=discord.Embed().from_dict(thing))
         except Exception as e:
             raise commands.CommandError(e)
-
-    @commands.group(invoke_without_command=True, aliases=["findsource"])
-    async def rtfs(self, ctx, *, thing: str = None):
-        await self.uhh_rtfs_pls(ctx, "discord.py", thing)
-        
 
     @commands.group(invoke_without_command=True,
                     aliases=[
