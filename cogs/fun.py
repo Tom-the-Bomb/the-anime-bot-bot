9fd84a18701025f752ab38ba3c77ce11e844dbf5
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-10 11:29:19 -0800

    oops

--- cogs/fun.py
+++ cogs/fun.py
@@ -215,7 +215,7 @@ class fun(commands.Cog):
         async with self.bot.session.get("https://captionbot.azurewebsites.net/api/messages", headers={"Content-Type": "application/json; charset=utf-8"}, data=json.dumps(data)) as resp:
             text = await resp.text()
             embed = discord.Embed(color=self.bot.color, title=text)
-            embed.set_url(url="attachment://caption.png")
+            embed.set_image(url="attachment://caption.png")
             async with self.bot.session.get(url) as resp:
                 bytes_ = BytesIO(await resp.read())
             await ctx.send(embed=embed, file=discord.File(bytes_, "caption.png"))
