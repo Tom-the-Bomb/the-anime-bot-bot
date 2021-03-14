4c575011d5e01486a6d821709eec91c32e04dc70
Jadon <47833807+WhoTheOOF@users.noreply.github.com>
2021-03-13 20:15:57 -0600

    Delete cogs directory

--- cogs/jishaku.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-from jishaku.cog import STANDARD_FEATURES, OPTIONAL_FEATURES
-
-class jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
-    pass
-
-def setup(bot):
-    bot.add_cog(jishaku(bot=bot))
