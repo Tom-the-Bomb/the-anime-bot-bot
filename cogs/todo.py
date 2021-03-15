3b857fec93975cff39d38810e19263d36e29df83
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-15 10:34:51 -0700

    Update todo.py

--- cogs/todo.py
+++ cogs/todo.py
@@ -39,8 +39,8 @@ class todo(commands.Cog):
         await pages.start(ctx)
     @todo.command()
     async def add(self, ctx, *, content):
-        await self.bot.db.execute("INSERT INTO todos (author_id, content, created_at, message_id, jump_url) VALUES ($1, $2, $3, $4, $5)", ctx.author.id, content, ctx.message.created_at, ctx.message.id, ctx.message.jump_url)
-        return await ctx.send(embed=discord.Embed(color=self.bot.color, description="Successfully added new todo"))
+        todos = await self.bot.db.fetch("INSERT INTO todos (author_id, content, created_at, message_id, jump_url) VALUES ($1, $2, $3, $4, $5) RETURNING *", ctx.author.id, content, ctx.message.created_at, ctx.message.id, ctx.message.jump_url)
+        return await ctx.send(embed=discord.Embed(color=self.bot.color, title="Successfully added new todo", description=f"{len(todos)} - {content}"))
 
         
 def setup(bot):
