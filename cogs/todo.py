2c18a1af0e62f876390aed9109d2f352e8b72875
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-15 09:01:13 -0700

    Update todo.py

--- cogs/todo.py
+++ cogs/todo.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
+from menus import menus
 from jishaku.paginators import PaginatorEmbedInterface, PaginatorInterface
+
+class TodoMenuSource(menus.ListPageSource):
+    def __init__(self, data):
+        super().__init__(data, per_page=4)
+    async def format_page(self, menu, entries):
+        return {"embed": discord.Embed(f"{menu.ctx.author.name}'s menu", description="\n".join(entries))}
 
 class todo(commands.Cog):
     def __init__(self, bot):
@@ -14,13 +21,13 @@ class todo(commands.Cog):
         todos = await self.bot.db.fetch("SELECT * FROM todos WHERE author_id = $1 ORDER BY created_at", ctx.author.id)
         if not todos:
             return await ctx.send(embed=discord.Embed(color=self.bot.color, description=f"you have no todos `{ctx.prefix}todo add sometodos` to make one"))
-        paginator = commands.Paginator(prefix="", suffix="", max_size=1000)
+        lists = []
         counter = 1
         for i in todos:
-            paginator.add_line(f"[{counter}]({i['jump_url']}). {i['content']}")
+            lists.append(f"[{counter}]({i['jump_url']}). {i['content']}")
             counter += 1
-        interface = PaginatorEmbedInterface(ctx.bot, paginator, owner=ctx.author)
-        await interface.send_to(ctx)
+        pages = menus.MenuPages(source=TodoMenuSource(lists))
+        await pages.start(ctx)
     @todo.command()
     async def add(self, ctx, *, content):
         await self.bot.db.execute("INSERT INTO todos (author_id, content, created_at, message_id, jump_url) VALUES ($1, $2, $3, $4, $5)", ctx.author.id, content, ctx.message.created_at, ctx.message.id, ctx.message.jump_url)
