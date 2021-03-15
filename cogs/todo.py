c2effb7ef134924e5419ab4031f517aef2909417
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-15 10:54:53 -0700

    Update todo.py

--- cogs/todo.py
+++ cogs/todo.py
@@ -50,7 +50,7 @@ class todo(commands.Cog):
             await self.bot.db.execute("INSERT INTO todos (author_id, content, created_at, message_id, jump_url) VALUES ($1, $2, $3, $4, $5)", ctx.author.id, i, ctx.message.created_at + datetime.timedelta(microsecond=offset), ctx.message.id, ctx.message.jump_url)
             offset += 1
         todos = await self.bot.db.fetch("SELECT * FROM todos WHERE author_id = $1 AND message_id = $2", ctx.author.id, ctx.message.id)
-        return await ctx.send(embed=discord.Embed(color=self.bot.color, title="Successfully added new todos", description=f"{'\n'.join(tasks)}")
+        return await ctx.send(embed=discord.Embed(color=self.bot.color, title="Successfully added new todos", description=f"{'\n'.join(tasks)}"))
         
         
     @todo.command()
