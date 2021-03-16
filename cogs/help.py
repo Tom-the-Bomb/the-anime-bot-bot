b6ac84b07a3514e6c6b0ff31c1d11f5d2ac48d0e
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-16 10:38:30 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -30,8 +30,13 @@ class HelpCommand(commands.HelpCommand):
     async def send_cog_help(self, cog):
         lists = []
         commands_ = await self.filter_commands(cog.get_commands(), sort=True)
-        commands_ = [self.get_command_signature(i) for i in commands_]
         for i in commands_:
+            if isinstance(i, commands.Group):
+                for v in i.walk_commands():
+                    lists.append(self.get_command_signature(v)
+            else:
+                self.get_command_signature(i0
+        for i in lists:
             lists.append(f"**{i}**")
         pages = menus.MenuPages(source=HelpMenuSource(lists), clear_reactions_after=True)
         await pages.start(self.context)
