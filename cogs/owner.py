472c9fc4b993fc3662cf3cbb85550ca5e45d918e
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-16 22:18:48 -0700

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -71,7 +71,7 @@ class owners(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def ss(self, ctx, website:str):
-        driver = webdriver.Firefox(options=Options)
+        driver = webdriver.Firefox()
         await self.bot.loop.run_in_executor(None, driver.get, website)
         bytes_ = BytesIO(driver.get_screenshot_as_png())
         file = discord.File(bytes_, filename="screnshot.png")
