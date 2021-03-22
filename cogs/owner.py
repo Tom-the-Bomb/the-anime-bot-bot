26c5f093c6587f272be2d4fb070d485a596ae852
Jadon <47833807+WhoTheOOF@users.noreply.github.com>
2021-03-21 22:24:54 -0500

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -406,9 +406,9 @@ class owners(commands.Cog):
         is_multistatement = query.count(';') > 1
         if is_multistatement:
             # fetch does not support multiple statements
-            strategy = self.db.execute
+            strategy = self.bot.db.execute
         else:
-            strategy = self.db.fetch
+            strategy = self.bot.db.fetch
 
         try:
             start = time.perf_counter()
