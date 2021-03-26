75a8e319eabf7ecb6ffd4956cecf2eef30957e4e
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-26 14:00:07 -0700

    smh

--- cogs/logging.py
+++ cogs/logging.py
@@ -240,7 +240,7 @@ class logging(commands.Cog):
     @commands.Cog.listener()
     async def on_raw_message_edit(self, payload):
         if payload.cached_message:
-            RETURNING
+            return
         if not payload.data.get("guild_id"):
             return
         embed = discord.Embed(color=self.bot.color, title="Message Edited", description=f"The message is too old I can't find the content", timestamp=datetime.datetime.utcnow())
