039f5a62dfe7fd4b240550b3fedc10cd76633fd7
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-27 10:04:58 -0700

    ree slash

--- cogs/slash.py
+++ cogs/slash.py
@@ -10,11 +10,21 @@ class slash(commands.Cog):
 
     @cog_ext.cog_slash(name="ping")
     async def ping(self, ctx):
+        start = time.perf_counter()
+        await ctx.defer()
+        end = time.perf_counter()
+        final_latency = end - start
+        start=time.perf_counter()
+        await self.bot.db.fetch("SELECT 1")
+        postgres = time.perf_counter()-start
+        postgres = round(postgres*1000)
         embed = discord.Embed(color=self.bot.color)
         embed.set_author(name="ping")
         embed.add_field(name="<:stab:744345955637395586>  websocket latency",
                         value=f"```{round(self.bot.latency * 1000)} ms ```")
-        await ctx.respond()
+        embed.add_field(name="<:postgres:821095695746203689> Postgre sql latency", value=f"```{postgres} ms```")
+        embed.add_field(name="<a:typing:597589448607399949> API latency",
+                        value=f"```{round(final_latency * 1000)} ms ```")
         await ctx.send(embed=embed)
 
 def setup(bot):
