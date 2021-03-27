bbbe70dbe426d8a347e7b0cd593a4ca1a01804ce
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-27 13:28:10 -0700

    :c

--- cogs/help.py
+++ cogs/help.py
@@ -30,7 +30,6 @@ class HelpCommand(commands.HelpCommand):
         await self.context.send(embed=embed)
 
     async def send_cog_help(self, cog):
-        await self.context.send("sending cog help")
         lists = []
         commands_ = await self.filter_commands(cog.get_commands(), sort=True)
         lists_ = []
@@ -40,6 +39,7 @@ class HelpCommand(commands.HelpCommand):
                     lists_.append(self.get_command_signature(v))
             else:
                 lists_.append(self.get_command_signature(i))
+        await self.context.send(lists_)
         for i in lists_:
             lists.append(f"**{i}**")
         pages = menus.MenuPages(source=HelpMenuSource(lists), delete_message_after=True)
