0ae6a95a3f5c2a55f63b525d79e19382f9ff9ca7
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-30 14:46:32 -0700

    Update pictures.py

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -378,7 +378,8 @@ class pictures(commands.Cog):
         async with ctx.channel.typing():
             url = await self.get_url(ctx, thing)
             image = await self.bot.zaneapi.braille(url)
-            await ctx.send(image[:-10])
+            num = len(image) - 2000
+            await ctx.send(image[:-num])
 
     @commands.command(aliases=["dot"])
     async def dots(self,
