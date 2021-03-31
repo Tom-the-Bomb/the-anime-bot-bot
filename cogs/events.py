187710475c9c99ce2b5e30b1368c18cdef196729
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-03-30 18:02:28 -0700

    oP

--- cogs/events.py
+++ cogs/events.py
@@ -483,11 +483,11 @@ class events(commands.Cog):
         print(len(self.bot.guilds))
         print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(self.bot))
         
-        @staticmethod
-        def embed(text):
-        return discord.Embed(color=0xFF0000,
-                              title="An error occured",
-                              description=text)
+    @staticmethod
+    def embed(text):
+    return discord.Embed(color=0xFF0000,
+                            title="An error occured",
+                            description=text)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
