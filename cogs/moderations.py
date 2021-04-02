7f6ab03953bfb80b7ce8e60f6a8b85ea3ebfdf87
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-04-01 20:40:20 -0700

    fix

--- cogs/moderations.py
+++ cogs/moderations.py
@@ -93,6 +93,7 @@ class moderations(commands.Cog):
             old_bad_words = self.bot.bad_word_cache[ctx.guild.id]
             old_bad_words.append(word)
             await self.bot.db.execute("UPDATE bad_words SET words = $2 WHERE guild_id = $1", ctx.guild.id, self.bot.bad_word_cache[ctx.guild.id])
+            await ctx.send("Success")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
