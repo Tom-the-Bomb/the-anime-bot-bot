1b7fef9a6ebe1a9e0102c918c29b717e888d7e5a
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-04-01 19:02:05 -0700

    a

--- cogs/piggo.py
+++ cogs/piggo.py
@@ -7,8 +7,21 @@ import datetime
 class reactionrole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.role_message_id = 812050921714089996
-        self.emoji_to_role = {
+        self.unnie_message_id = 826236025789349918
+        self.unnie_emoji_to_role = {
+            727995475117998111: 810336158008737792,
+            796132269312049152:,
+            798756695949705257:,
+            777815702437494784:,
+            658386618205470780:,
+            665825427197001743:,
+            741017487994519633:,
+            515993072031498260:,
+            588504238494187525:,
+            810349419802132481:
+        }
+        self.pig_role_message_id = 812050921714089996
+        self.pig_emoji_to_role = {
             701518910842732640: 701546882320826428,
             711089394031001600: 701543891119243324,
             658157341816127498: 701906891848024124,
@@ -30,23 +43,73 @@ class reactionrole(commands.Cog):
             691499259438170152: 811804360107360267
         }
 
-    def cog_check(self, ctx):
-        return bool(ctx.guild and ctx.guild.id == 701251070977900584)
 
     @commands.Cog.listener()
-    async def on_raw_message_delete(self, payload):
-        if payload.guild_id == 701251070977900584:
-            msg = self.bot.deleted_message_cache.get(payload.message_id)
-            if not msg:
-                return
-            embed = discord.Embed(color=self.bot.color)
-            embed.set_author(name=msg.author.display_name,
-                             icon_url=str(msg.author.avatar_url))
-            embed.add_field(name="Message delete",
-                            value=f"Message: {msg.content}")
-            await self.bot.get_channel(812054734919303189).send(embed=embed)
-            self.bot.deleted_message_cache.pop(payload.message_id)
+    async def on_raw_reaction_add(self, payload):
+        """Gives a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about
+        if payload.message_id != self.pig_role_message_id:
+            return
 
+        try:
+            role_id = self.pig_emoji_to_role[payload.emoji.id
+                                         or payload.emoji.name]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally add the role
+            await payload.member.add_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        """Removes a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about
+        if payload.message_id != self.pig_role_message_id:
+            return
+
+        try:
+            role_id = self.pig_emoji_to_role[payload.emoji.id
+                                         or payload.emoji.name]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Makes sure the member still exists and is valid
+            return
+
+        try:
+            # Finally, remove the role
+            await member.remove_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         """Gives a role based on a reaction emoji."""
