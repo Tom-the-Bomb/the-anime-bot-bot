13fe4bd0923e96f9788be2f9367f64be19f599a1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-04-04 16:49:17 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -537,7 +537,7 @@ class utility(commands.Cog):
                         else js["message"]
                     )
                 result = f"```{lang}\n{js['output'].replace('code.code', 'cogs.eval')}\n```"
-                if result >= 1900 or len(result.split("\n")) >= 40:
+                if len(result) >= 1900 or len(result.split("\n")) >= 40:
                     return await ctx.send(await ctx.paste(result))
                 
                 return await ctx.send(
