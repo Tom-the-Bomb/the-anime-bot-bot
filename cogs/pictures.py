7af35e8dcc9f7b26258d6ea91739c6f994c68c39
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-04-24 12:20:36 -0700

    Update pictures.py

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -231,7 +231,6 @@ class pictures(commands.Cog):
                                 format="GIF",
                                 append_images=to_make_gif[1:],
                                 save_all=True,
-                                disposal=2,
                                 duration=img.info["duration"],
                                 loop=0,
                             )
