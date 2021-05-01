74d8414b391607d0c0d248066d1c9fe1dc40687f
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-01 14:05:36 -0700

    o

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -34,6 +34,7 @@ class Reminder(commands.Cog):
     async def wait_for_timers(self, timer):
         if timer.time > datetime.datetime.utcnow():
             await asyncio.sleep((timer.time - datetime.datetime.utcnow()).total_seconds())
+            await self.bot.get_channel(timer.channel_id).send("i waited")
             self.bot.dispatch("timer_complete", timer)
             await self.bot.db.execute("DELETE FROM reminder WHERE id = $1", timer.id)
 
