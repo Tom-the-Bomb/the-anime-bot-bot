44c33206710b021ccc8da861c62288bec254e221
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-02 12:22:32 -0700

    o

--- cogs/events.py
+++ cogs/events.py
@@ -628,7 +628,7 @@ class events(commands.Cog):
         elif isinstance(error, PIL.UnidentifiedImageError):
             embed = self.embed("No image found")
             await ctx.reply(embed=embed)
-        elif isinstance(error, PIL.DecompressionBombWarning):
+        elif isinstance(error, PIL.DecompressionBombError):
             embed = self.embed("eww decompression bomb eww stop or i use my ban hammer")
             await ctx.reply(embed=embed)
         elif isinstance(error, aiozaneapi.GatewayError):
