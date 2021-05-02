8c2ae245dcf115535231e21705f56a10933a1ae7
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-02 13:45:05 -0700

    o

--- utils/embed.py
+++ utils/embed.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 class embedbase:
   async def embed(self, ctx):
-    embed = discord.Embed(color=0x2ecc71, timestamp=datetime.utcnow())
-    embed.set_footer(text=f"requested by {ctx.author} response time : {round(self.bot.latency * 1000)} ms", icon_url=ctx.author.avatar_url)
+    embed = discord.Embed(color=ctx.bot.color, timestamp=datetime.utcnow())
+    embed.set_footer(text=f"requested by {ctx.author}", icon_url=ctx.author.avatar_url)
     return embed
