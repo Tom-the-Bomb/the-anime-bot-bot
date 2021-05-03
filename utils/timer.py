f46fe14444d4307b52fafaf32b6594d4df985ee7
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-02 22:29:10 -0700

    Update timer.py

--- utils/timer.py
+++ utils/timer.py
@@ -16,6 +16,13 @@ class Timer:
 		self.end = round(time, 3)
 		return f"{self.end} ms"
 	
+	async def __aenter__(self, *args, **kwargs):
+		self.start()
+		return self
+     
+	async def __aexit__(self, *args, **kwargs):
+		self.stop()
+	
 	def __enter__(self, *args, **kwargs):
 		self.start()
 		return self
