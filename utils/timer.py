e7539d740f9dc5f36be8efcba538fbca5da5a218
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-02 22:35:30 -0700

    Update timer.py

--- utils/timer.py
+++ utils/timer.py
@@ -1,25 +1,37 @@
 import time
 
 class Timer:
-	def __init__(self):
-		self._start = None
-		self._end = None
-	
-	def start(self):
-		self._start = time.perf_counter()
+    def __init__(self):
+        self._start = None
+        self._end = None
 
-	def stop(self):
-		self._end = time.perf_counter()
+    def start(self):
+        self._start = time.perf_counter()
 
-	def __str__(self):
-		time_= (self._end - self._start) * 1000
-		self.end = round(time_, 3)
-		return f"{self.end} ms"
-	
-	def __enter__(self, *args, **kwargs):
-		self.start()
-		return self
-     
-	def __exit__(self, *args, **kwargs):
-		self.stop()
-		
+    def stop(self):
+        self._end = time.perf_counter()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def __int__(self):
+        return round(self.time, 3)
+
+    def __float__(self):
+        return self.time
+
+    def __str__(self):
+        return str(self.time)
+
+    def __repr__(self):
+        return f"<Timer time={self.time}>"
+
+    @property
+    def time(self):
+        if self._end is None:
+            raise ValueError("Timer has not been ended.")
+        return self._end - self._start
