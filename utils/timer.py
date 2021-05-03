e071d554a79fa44bd5dd77d5976e732a5a4cf4a3
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-02 22:20:38 -0700

    Update timer.py

--- utils/timer.py
+++ utils/timer.py
@@ -2,18 +2,24 @@ import time
 
 class Timer:
 	def __init__(self):
-		self.time = None
-		self.start = None
-		self.end = None
-		
+		self._start = None
+		self._end = None
+	
+	def start(self):
+		self._start = time.perf_counter()
+
+	def stop(self):
+		self._end = time.perf_counter()
+
 	def __str__(self):
+		time = (self._end - self._start) * 1000
+		self.end = round(time, 3)
 		return f"{self.end} ms"
 	
 	def __enter__(self, *args, **kwargs):
-		self.start = time.perf_counter()
+		self.start()
 		return self
      
 	def __exit__(self, *args, **kwargs):
-		t = time.perf_counter()
-		time = (t - self.start) * 1000
-		self.end = round(, 3)
+		self.stop()
+		
