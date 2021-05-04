60e5ef09c81b10c973bfa36434a6c020de3f1201
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-03 17:15:48 -0700

    Update chat.py

--- cogs/chat.py
+++ cogs/chat.py
@@ -15,7 +15,7 @@ class chat(commands.Cog):
     
     @asyncexe()
     def wordcloud_(self, text):
-        wordcloud = WordCloud(width=600, height=400).generate(text)
+        wordcloud = WordCloud(width=800, height=400).generate(text)
         image = wordcloud.to_image()
         b = BytesIO()
         image.save(b, "PNG")
