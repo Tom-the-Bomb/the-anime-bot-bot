7a0c044d0c5147ef6a554d270fd36650529c5621
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-05 12:11:39 -0700

    Update pictures.py

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -606,7 +606,7 @@ class pictures(commands.Cog):
     @commands.command()
     async def latex(self, ctx, *, text):
         async with self.bot.session.get("https://chart.googleapis.com/chart", params={"cht": "tx", "chl": quote(text), "chf": "a, s, 00000000", "chs": "200"}) as resp:
-            await ctx.send(file=discord.File(await asyncio.to_thread(self.process_latex(), BytesIO(await resp.read())), "The_Anime_Bot_latex.png"))
+            await ctx.send(file=discord.File(await asyncio.to_thread(self.process_latex, BytesIO(await resp.read())), "The_Anime_Bot_latex.png"))
             
     @commands.command()
     async def spin(
