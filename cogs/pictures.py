ad82f57378fae4caf16a31fc00971f8744d79c8c
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-05 22:15:09 -0700

    o

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -622,11 +622,11 @@ class pictures(commands.Cog):
         faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
+            roi_gray = gray_img[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (255,0,0), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
         is_success, im_buf_arr = cv2.imencode(".png", img)
         return discord.File(BytesIO(im_buf_arr), "The_Anime_Bot_Face_Reg.png")
 
