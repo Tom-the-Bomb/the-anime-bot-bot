c4b6aad3cdc224ae511481c9f4b10e9b7e42a988
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-05 18:10:09 -0700

    o

--- cogs/owner.py
+++ cogs/owner.py
@@ -84,7 +84,7 @@ class owners(commands.Cog):
 
     @commands.command()
     async def hahafile(self, ctx, files: int = 1):
-        b = b"\x00"*(guild.filesize_limit - 100)
+        b = b"\x00"*(ctx.guild.filesize_limit - 100)
         [
             self.bot.loop.create_task(
                 ctx.send(file=discord.File(BytesIO(b), f"thing{i}.somethingy"))
