517ca537f9278be951a06569fad0e6d8549e808d
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-05 22:17:19 -0700

    o

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -618,7 +618,7 @@ class pictures(commands.Cog):
         img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         haar_face_cascade = cv2.CascadeClassifier("/usr/local/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_alt.xml")
-        eye_cascade = cv2.CascadeClassifier("/usr/local/lib/python3.9/site-packages/cv2/data/haarcascade_eye.xml")
+        eye_cascade = cv2.CascadeClassifier("/usr/local/lib/python3.9/site-packages/cv2/data/haarcascade_eye_tree_eyeglasses.xml")
         faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
