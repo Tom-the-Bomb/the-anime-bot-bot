ca6cceed3f78f24b3675f022663736d62202ee4e
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-11 17:30:49 -0700

    o

--- cogs/votemanager.py
+++ cogs/votemanager.py
@@ -7,6 +7,7 @@ import asyncio
 import time
 from contextlib import suppress
 import aiohttp
+from aiohttp import web
 import quart
 from quart import abort
 from hypercorn.asyncio import serve
@@ -20,23 +21,27 @@ from quart import Quart
 class VoteManager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    #     self.app = app
+        self.app = web.Application(print=None)
+        self.bot.loop.create_task(self.run())
+
+    def cog_unload(self):
+        self.bot.loop.create_task(self._webserver.stop())
+    
+    async def index(self):
+        return web.Response(text="hello o")
+    
+
+    async def run(self):
+        self.app.router.add_get("/", index)
+        runner = web.AppRunner(self.app)
+        await runner.setup()
+        self._webserver = web.TCPSite(runner, "0.0.0.0", "50000")
+        await self._webserver.start()
+
     
     # @app.route("/")
     # async def index(self):
     #     return {"hello": "o"}
-    
-    # @app.route("/topgg")
-    # async def topgg(self):
-    #     auth = request.headers["Authorization"]
-    #     if auth != config.vote_webhook_token:
 
-def teardown(bot):
-    # app.shutdown()
-    # shut_down.set()
-    ...
 def setup(bot):
     bot.add_cog(VoteManager(bot))
-    # # config = Config()
-    # # config.bind = ["0.0.0.0:50000"]
-    # bot.loop.create_task(app.run("0.0.0.0", "50000"))
