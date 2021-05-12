21ca7a2ada1ef37d6e0ce55a64fd87628d87c6e5
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-11 18:08:02 -0700

    o

--- cogs/votemanager.py
+++ cogs/votemanager.py
@@ -27,12 +27,38 @@ class VoteManager(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self._webserver.stop())
     
+    async def topgg(self, request):
+        if not request.headers.get("Authorization"):
+            return web.Response(status=401, "Unauthorized")
+        if request.headers.get("Authorization") != config.vote_webhook_token:
+            return web.Response(status=401, "Unauthorized")
+        data = await request.json()
+        try:
+            user = await self.bot.getch(data["user"])
+            vote_counts = await self.bot.db.fetchval(
+                (
+                    "INSERT INTO votes VALUES ($1, $2)"
+                    " ON CONFLICT (user_id) "
+                    "DO UPDATE count = votes.count + 1"
+                    " RETURNING count"
+                ),
+                user.id,
+                1,
+            )
+            await user.send(f"Hey, {user.mention} Thanks for voting it mean a lot this is your {vote_counts} voting for The Anime Bot thank you so much.")
+            await self.bot.get_channel(791518421920907265).send(f"{user.mention}, just upvoted our bot this is their {vote_counts} voting for The Anime Bot")
+        except:
+            return web.Response(status=500)
+        return web.Response(status=200, text="OK")
+
     async def index(self, request):
-        return web.Response(text="hello o")
+        return web.Response(text="anime bot is the best bot")
     
 
     async def run(self):
+        await self.bot.wait_until_ready()
         self.app.router.add_get("/", self.index)
+        self.app.router.add_post("/topgg", self.topgg)
         runner = web.AppRunner(self.app)
         await runner.setup()
         self._webserver = web.TCPSite(runner, "0.0.0.0", "15000")
