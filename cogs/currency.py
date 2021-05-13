9315013c5a09f9b1eb44ee34a38f3d01605f514f
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-12 17:36:02 -0700

    O

--- cogs/currency.py
+++ cogs/currency.py
@@ -20,20 +20,20 @@ class Currency(commands.Cog):
     
     async def open_account(self, user_id):
         try:
-            await self.bot.db.execute("INSERT INTO economy VALUES ($1, $2, $2, $2)", user_id, 0)
+            await self.bot.db.execute("INSERT INTO economy VALUES ($1, $2, $2, $2)", user_id, "0")
             return True
         except asyncpg.UniqueViolationError:
             return False
 
     async def get_balance(self, user_id):
         bal = await self.bot.db.fetchrow("SELECT * FROM economy WHERE user_id = $1", user_id)
-        return bal["basket"], bal["bank"]
+        return int(bal["basket"]), int(bal["bank"])
 
     async def change_balance(self, user_id, amount, type_="basket"):
         if type_ == "basket":
-            return await self.bot.db.fetchval("UPDATE economy SET basket = $2 + basket WHERE user_id = $1 RETURNING basket", user_id, amount)
+            return int(await self.bot.db.fetchval("UPDATE economy SET basket = $2 + basket WHERE user_id = $1 RETURNING basket", user_id, str(amount)))
         else:
-            return await self.bot.db.fetchval("UPDATE economy SET bank = $2 + bank WHERE user_id = $1 RETURNING bank", user_id, amount)
+            return int(await self.bot.db.fetchval("UPDATE economy SET bank = $2 + bank WHERE user_id = $1 RETURNING bank", user_id, str(amount)))
     
     @commands.command(aliases=["bal"])
     async def balance(self, ctx):
