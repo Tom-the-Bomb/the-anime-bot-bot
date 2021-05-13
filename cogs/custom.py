d741a309cc39f3a215ce23eec8a9389c0e67cd4a
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-13 16:29:17 -0700

    o

---
+++ cogs/custom.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from utils.subclasses import AnimeContext
+import copy
+import datetime
+
+
+class Custom(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.guild.id == 786359602241470464:
+            await member.add_roles(discord.Object(792645158495453204))
+        if member.guild.id == 796459063982030858:
+            await member.add_roles(discord.Object(833132759361912842))
+        
+
+def setup(bot):
+    bot.add_cog(reactionrole(bot))
