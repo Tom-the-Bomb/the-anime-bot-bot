a7dee11e36013e8810f9d86e1337a22361fbf26d
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-13 12:38:00 -0700

    o

--- cogs/currency.py
+++ cogs/currency.py
@@ -32,6 +32,9 @@ class Economy(commands.Cog):
     async def change_balance(self, user_id, amount, type_="basket"):
         await self.open_account(user_id)
         basket, bank = await self.get_balance(user_id)
+        total_earned = await self.bot.db.fetval("SELECT total_earned FROM economy WHERE user_id = $1", user_id)
+        if amount > 0:
+            await self.bot.db.execute("UPDATE economy SET total_earned = $2 WHERE user_id = $1", user_id, int(total_earned) + int(amount))
         if type_ == "basket":
             return int(await self.bot.db.fetchval("UPDATE economy SET basket = $2 WHERE user_id = $1 RETURNING basket", user_id, str(amount + basket)))
         else:
