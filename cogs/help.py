6c9e62b56323e2a6fe9e3c51d4ce5011fba1708d
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-18 12:16:28 -0700

    o

--- cogs/help.py
+++ cogs/help.py
@@ -24,6 +24,48 @@ class HelpMenuSource(menus.ListPageSource):
 
 
 class HelpCommand(commands.HelpCommand):
+
+    async def command_callback(self, ctx, *, command=None):
+        await self.prepare_help_command(ctx, command)
+        bot = ctx.bot
+
+        if command is None:
+            mapping = self.get_bot_mapping()
+            return await self.send_bot_help(mapping)
+
+        # Check if it's a cog
+        for cog in bot.cogs.keys():
+            if cog.lower() == command.lower():
+                return await self.send_cog_help(bot.get_cog(cog))
+
+        maybe_coro = discord.utils.maybe_coroutine
+
+        # If it's not a cog then it's a command.
+        # Since we want to have detailed errors when someone
+        # passes an invalid subcommand, we need to walk through
+        # the command group chain ourselves.
+        keys = command.split(' ')
+        cmd = bot.all_commands.get(keys[0])
+        if cmd is None:
+            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
+            return await self.send_error_message(string)
+
+        for key in keys[1:]:
+            try:
+                found = cmd.all_commands.get(key)
+            except AttributeError:
+                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                return await self.send_error_message(string)
+            else:
+                if found is None:
+                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    return await self.send_error_message(string)
+                cmd = found
+
+        if isinstance(cmd, Group):
+            return await self.send_group_help(cmd)
+        else:
+            return await self.send_command_help(cmd)
     def get_command_signature(self, command):
         return f"{self.clean_prefix}{command.qualified_name} {command.signature}"
 
