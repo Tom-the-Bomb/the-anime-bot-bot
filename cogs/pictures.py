b6da19a63f1919f28663fa9a5ba0359227da7432
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-19 18:42:07 -0700

    smh

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -1398,9 +1398,11 @@ class pictures(commands.Cog):
         glitcher = ImageGlitcher()
         with Image.open(image) as img:
             if img.is_animated and img.n_frames < 200:
-                glitch_img = glitcher.glitch_gif(img, intensity, color_offset=True, gif=True)
+                to_make_gif = []
+                for im in ImageSequence.Iterator(img):
+                    to_make_gif.append(glitcher.glitch_gif(img, intensity, color_offset=True))
                 final = BytesIO()
-                self.save_transparent_gif(glitch_img, img.info["durations"])
+                self.save_transparent_gif(glitch_img, img.info["durations"], final)
                 final.seek(0)
                 return discord.File(final, "The_Anime_Bot_glitch.gif")
             else:
