6bacf2c3a4f9a39be60d241b860b25d15ec36461
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-05-21 12:45:25 -0700

    oh

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -23,6 +23,7 @@ import ujson
 import qrcode
 from utils.subclasses import AnimeContext
 from qrcode.image.pure import PymagingImage
+import imutils
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
@@ -73,6 +74,22 @@ Image_Union = typing.Union[
     str,
 ]
 
+class ShapeDetector:
+	def detect(self, c):
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        if len(approx) == 3:
+			shape = "triangle"
+		elif len(approx) == 4:
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+		elif len(approx) == 5:
+			shape = "pentagon"
+		else:
+			shape = "circle"
+		return shape
 
 class ColorConverter(commands.Converter):
     async def convert(self, ctx, argument):
@@ -745,6 +762,49 @@ class pictures(commands.Cog):
         }
         async with self.bot.session.post(url, json=bobo) as resp:
             await ctx.reply(file=discord.File(BytesIO(await resp.read()), "The_Anime_Bot_code.png"))
+
+    @asyncexe()
+    def shape_detection(self, image):
+        with Image.open(image) as img:
+            buffer = BytesIO()
+            img_ = img.convert("RGB")
+            img_.save(buffer, "PNG")
+            img_.close()
+            buffer.seek(0)
+        np_array = np.asarray(bytearray(buffer.read()), dtype=np.uint8)
+        image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        resized = imutils.resize(image, width=300)
+        ratio = image.shape[0] / float(resized.shape[0])
+        image = cv2.cvtColor(ratio, cv2.COLOR_BGR2GRAY)
+        image = cv2.GaussianBlur(image, (5, 5), 0)
+        image = cv2.threshold(image, 60, 255, cv2.THRESH_BINARY)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	    cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        sd = ShapeDetector()
+        for c in cnts:
+            M = cv2.moments(c)
+            cX = int((M["m10"] / M["m00"]) * ratio)
+            cY = int((M["m01"] / M["m00"]) * ratio)
+            shape = sd.detect(c)
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
+            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        is_success, im_buf_arr = cv2.imencode(".png", image)
+        b = BytesIO(im_buf_arr)
+        return discord.File(b, "The_Anime_Bot_object_detection.png")
+    
+    @commands.command()
+    async def objectdetection(self, ctx, thing: Image_Union):
+        async with ctx.channel.typing():
+            url = await self.get_url(ctx, thing)
+            async with self.bot.session.get(url) as resp:
+                b = BytesIO(await resp.read())
+            await ctx.reply(file=await self.objectdetection(b))
+
+        
 
     @asyncexe()
     def facereg_(self, image):
