3e3932e38f6d9e3b10af0a5422ea135e3896ab43
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-14 19:09:39 -0700

    smh

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -12,7 +12,7 @@ class Emoji(commands.Cog):
         self.bot = bot
         self.to_save = {}
         self.save_lock = asyncio.Lock()
-        self.save_stats.start()
+        self._task = self.save_stats.start()
     
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -31,12 +31,15 @@ class Emoji(commands.Cog):
                     continue
                 c = self.to_save[message.guild.id].get(i, 0)
                 self.to_save[message.guild.id][i] = c + 1
+
+    def cog_unload(self):
+        self._task.cancel()
     
     @tasks.loop(seconds=10)
     async def save_stats(self):
         await self.bot.wait_until_ready()
         async with self.save_lock:
-            sql = "INSERT INTO emoji_stats VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET emojis = $2"
+            sql = "INSERT INTO emoji_stats VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET emojis = merge_json(emojis, $2)"
             for i, v in self.to_save.items():
                 j = ujson.dumps(v, ensure_ascii=True, escape_forward_slashes=False)
                 await self.bot.db.execute(sql, i, j)
