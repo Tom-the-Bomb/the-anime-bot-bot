aad51b25c8d5de47f61bd73cae705da7899e5551
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-14 19:35:09 -0700

    o

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -42,7 +42,7 @@ class Emoji(commands.Cog):
             sql = "INSERT INTO emoji_stats VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET emojis = $2"
             async with self.bot.db.acquire() as con:
                 for i, v in self.to_save.items():
-                    v.update(ujson.loads(await con.fetchval("SELECT emojis FROM emoji_stats WHERE guild_id = $1", i)))
+                    v.update(await con.fetchval("SELECT emojis FROM emoji_stats WHERE guild_id = $1", i))
                     j = ujson.dumps(v, ensure_ascii=True, escape_forward_slashes=False)
                     await con.execute(sql, i, j)
             self.to_save = {}
