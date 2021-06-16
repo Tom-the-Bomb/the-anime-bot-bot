31d9ed50e347b86b2a2a5b75508895fb8ce7a6d4
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-16 12:28:09 -0700

    smh

--- cogs/fun.py
+++ cogs/fun.py
@@ -706,7 +706,10 @@ class Fun(commands.Cog):
         c = discord.utils.find(lambda x: x.guild.id == ctx.guild.id, self.bot.voice_clients)
         if not c:
             c = await ctx.author.voice.channel.connect()
-        await c.move_to(ctx.author.voice.channel)
+        if c.channel.id != ctx.author.voice.channel.id:
+            await c.move_to(ctx.author.voice.channel)
+        if not c.is_connected():
+            await c.connect()
 
         buffer = await self.tts_(text, lang)
         if c.is_playing():
