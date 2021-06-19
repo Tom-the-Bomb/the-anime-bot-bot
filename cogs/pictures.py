eb5986b4780afb0c4d3fccfae10d1567f9903b75
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-18 18:42:07 -0700

    :O

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -592,7 +592,7 @@ class Images(commands.Cog):
         async with Processing(ctx):
             url = await self.get_url(ctx, thing)
             async with self.bot.session.get(url) as resp:
-                b, format = await self.process_gif((await resp.read()), lambda x: x.resize(width, height))
+                b, format = await self.process_gif((await resp.read()), lambda x: x.resize((width, height)))
                 await ctx.send(file=discord.File(b, f"The_Anime_Bot_resize.{format}"))
     
     @commands.command()
