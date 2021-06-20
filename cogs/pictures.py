d5ba1ca21b7edf8846d81a34ad3746fa5460db88
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-20 12:02:05 -0700

    ovo

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -154,7 +154,7 @@ class Images(commands.Cog):
         self.cdn_ratelimiter = ratelimiter.RateLimiter(max_calls=3, period=7)
         self.ocr_ratelimiter = ratelimiter.RateLimiter(max_calls=2, period=10)
 
-        excluded = ("glitch", "invert")
+        excluded = ("glitch", "invert", "magik")
 
         for name, func in inspect.getmembers(ImageFeatures):
             if name.startswith("_"):
